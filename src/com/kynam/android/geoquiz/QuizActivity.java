5bdb14fa0088aa26de87ddf4c91395dec0288b01
Nam <trankynam2612@gmail.com>
2014-05-19 21:54:06 +0700

    trang 48, truoc phan challenges

--- src/com/kynam/android/geoquiz/QuizActivity.java
+++ src/com/kynam/android/geoquiz/QuizActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Build;
 
@@ -17,19 +18,52 @@ public class QuizActivity extends ActionBarActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+    	new TrueFalse(R.string.question_ocean, true),
+    	new TrueFalse(R.string.question_mideast, false),
+    	new TrueFalse(R.string.question_africa, false),
+    	new TrueFalse(R.string.question_americas, true),
+    	new TrueFalse(R.string.question_asia, true),
+    };
+    
+    private int mCurrentIndex = 0;
+    
+    private void updateQuestion(){
+    	int question = mQuestionBank[mCurrentIndex].getQuestion();
+    	mQuestionTextView.setText(question);
+    }
+    
+    private void checkAnswer(boolean userPressTrue){
+    	boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+    	
+    	int messageResID = 0;
+    	
+    	if (userPressTrue == answerIsTrue) {
+    		messageResID = R.string.correct_toast;
+    	} else {
+    		messageResID = R.string.incorrect_toast;
+    	}
+    	
+    	Toast.makeText(this, messageResID, Toast.LENGTH_SHORT).show();
+    }
     
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+                
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+				checkAnswer(true);
 				
 			}
 		});
@@ -39,16 +73,29 @@ public class QuizActivity extends ActionBarActivity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+				checkAnswer(false);
 				
 			}
 		});
+        
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+        
+        updateQuestion();
 
-        if (savedInstanceState == null) {
+        /*if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-        }
+        }*/
     }
 
 
